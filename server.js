7f0fb48135622986546d684b14d7b88a53c007ae
NickCheng0921 <nickshacheng.0921@gmail.com>
2021-10-27 09:41:56 -0500

    Implemented echo feature for server

--- server.js
+++ server.js
@@ -6,16 +6,52 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8000 });
+var messages = [];
+var msgcounter;
 
 console.log("Server running");
 
 wss.on('connection', function connection(ws) {
+  let mymessages = [];
   ws.on('message', function incoming(message) {
     console.log('received: %s', decode_utf8(message));
+    let decode_arr = decode_msg(message);
+    let id = decode_arr[0];
+    let type = decode_arr[1];
+    let content = decode_arr[2];
+
+    //client is sending us a message
+    if(content == "01"){
+      //makes a random string of alphanumeric values
+      let msgid = Math.random().toString(16).substr(2, 8);
+      messages.push([msgid, id, content]);
+
+      let payload = msgid+"02"+id+" > "+content+'\n';
+      ws.send(encode_utf8(payload));
+    }
+
+    //client is requesting an update
+    if(content == "03"){
+      ;
+    }
+
   });
 
   //ws.send('something');
 });
+
+function decode_msg(msg){
+  let id = msg.substr(0, 8);
+  let type = msg.substr(8, 2);
+  let content = "";
+  try{
+    content = msg.substr(10);
+  }
+  catch (error){
+    console.log("Error reading a msg: ", msg);
+  }
+  return [id, type, content];
+}
 
 function encode_utf8(s) {
   return unescape(encodeURIComponent(s));
